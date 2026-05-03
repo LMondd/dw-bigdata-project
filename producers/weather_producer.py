c85c726c313104e811f5f20c534edb0727405924
LMondd <mond.phakapol@gmail.com>
2026-05-03 21:38:18 +0700

    feat: add OpenWeather Kafka producer

--- producers/weather_producer.py
+++ producers/weather_producer.py
@@ -25,7 +25,7 @@ CITIES = [
     {"name": "Bangkok", "city_id": 1609350},
     {"name": "Chiang Mai", "city_id": 1153671},
     {"name": "Phuket", "city_id": 1151254},
-    {"name": "Pattaya", "city_id": 1151182},
+    {"name": "Pattaya", "city_id": 1607981},
     {"name": "Chon Buri", "city_id": 1153673},
 ]
 
